c97f4073315cc562d3564b0cf53084f9af85b3a3
GOBI8450 <148621739+GOBI8450@users.noreply.github.com>
2024-07-04 13:06:37 +0300

    Update Main.py
    changed a lot of the structure and how to controll the game

--- Main.py
+++ Main.py
@@ -1,119 +1,136 @@
-import random
-import pygame, sys
-
-from ball_obj import Ball
-
-
-def update_text(ball):
-    return font.render('Ball 1 speed x=' + str(ball.ball_speed_x), True, 'white', bg_color), font.render(
-        'Ball 1 speed y=' + str(ball.ball_speed_y), True, 'white', bg_color)
-
-
-def moving_ball(ball_obj, ball2_obj):
-    ball_speed_x, ball_speed_y = ball_obj.ball_animation(ball2_obj)
-    return ball_speed_x, ball_speed_y
-
+import pygame, sys, random
+from BALLS import balls
 
 pygame.init()
 pygame.mixer.init()
 clock = pygame.time.Clock()
 
-#Setup sound for hit
-
+# Setup sound for hit
 
 screen_width = 1280
 screen_height = 980
 screen = pygame.display.set_mode((screen_width, screen_height))
-framer = 60
+fps = 60
 pygame.display.set_caption("tomySim")
 
-# Ball rectangle
 # Colors
 bg_color = pygame.Color('grey12')
 light_grey = (200, 200, 200)
 
 # Pyshics
-ball_speed_x = 0
-ball_speed_y = 0
-accelaration_y = 9.8
-opponent_speed = 7
-radius = 30
+gravity = 9.8
 
-ball_object = Ball(ball_speed_x=ball_speed_x, ball_speed_y=ball_speed_y, acceleration_y=accelaration_y, radius=radius,
-                   screen_height=screen_height, screen_width=screen_width, mass=1, mode=1)
-ball = ball_object.ball
-ball_object2 = Ball(ball_speed_x=ball_speed_x, ball_speed_y=ball_speed_y, acceleration_y=accelaration_y, radius=radius,
-                    screen_height=screen_height, screen_width=screen_width, mass=1, mode=12)
-ball2 = ball_object2.ball
+# Ball Attribute
+radius = 50
 
-# text:
-font = pygame.font.Font('freesansbold.ttf', 32)
-text_speed = font.render('', True, 'white', bg_color)
-textRect_x = text_speed.get_rect()
-textRect_y = text_speed.get_rect()
-textRect_x.center = (200, 100)
-textRect_y.center = (screen_height - 500, 100)
+balls_list = balls()
+balls_list.new_ball_exact_mass(gravity=0, radius=radius, mass=1, color=light_grey, screen=screen)
+main_ball = balls_list.balls[0]
 
-textRect_x2 = text_speed.get_rect()
-textRect_y2 = text_speed.get_rect()
-textRect_x2.center = (200, 200)
-textRect_y2.center = (screen_height - 400, 200)
+# Other features:
+fps_flag = True
+dragging = False
+touched_ball = False
+spawn_flag = False
+
+# Mouse speed configuration:
+max_speed_x = 0
+max_speed_y = 0
+mouse_speed_x, mouse_speed_y, max_speed_x, max_speed_y = 0, 0, 0, 0
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                dragging = True
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                print(mouse_x + main_ball.radius)
+                if max(main_ball.x, mouse_x) - min(main_ball.x, mouse_x) < radius and max(main_ball.y, mouse_y) - min(
+                        main_ball.y, mouse_y) < radius:
+                    touched_ball = True
+                else:
+                    touched_ball = False
+        if event.type == pygame.MOUSEMOTION:
+            if dragging and touched_ball:
+                mouse_speed_x, mouse_speed_y = event.rel
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                # Will Not Go More Than Bounderies
+                main_ball.x = max(main_ball.radius, min(mouse_x, screen_width - main_ball.radius))
+                main_ball.y = max(main_ball.radius, min(mouse_y, screen_height - main_ball.radius))
+        if event.type == pygame.MOUSEBUTTONUP:
+            dragging = False
+
+        # If you press a button:
         if event.type == pygame.KEYDOWN:
+            # Movement Keys:
+            if event.key == pygame.K_a:
+                spawn_flag = True
+            if event.key == pygame.K_h:
+                balls_list.new_ball_exact_pos(x=random.randint(0, screen_width), y=random.randint(0, screen_height),
+                                              gravity=gravity, radius=random.randint(10, 100),
+                                              color=(
+                                                  random.randint(0, 255), random.randint(0, 255),
+                                                  random.randint(0, 255)),
+                                              screen=screen)
+            if event.key == pygame.K_DOWN:
+                main_ball.ball_speed_y = 10
+            if event.key == pygame.K_UP:
+                main_ball.ball_speed_y = -10
             if event.key == pygame.K_RIGHT:
-                while ball_speed_x < 0:
-                    ball_speed_x += 1
-                ball_speed_x += 5
+                main_ball.ball_speed_x = 10
             if event.key == pygame.K_LEFT:
-                while ball_speed_x > 0:
-                    ball_speed_x -= 1
-                ball_speed_x -= 5
-            if event.key == pygame.K_0:
-                ball_object.ball_restart()
-                ball_object.update(ball_speed_x, ball_speed_y, ball)
-                print("0")
+                main_ball.ball_speed_x = -10
+            # Fetures Like Gravity Change:
             if event.key == pygame.K_9:
-                ball_object.acceleration_y *= -1
-                ball_object.update(ball_speed_x, ball_speed_y, ball)
-                print("Gravity changed")
-            if event.key == pygame.K_p:
-                ball_object.acceleration_y +=1
-                ball_object.update(ball_speed_x, ball_speed_y, ball)
-                print("Gravity +1")
-            if event.key == pygame.K_m:
-                ball_object.acceleration_y -= 1
-                ball_object.update(ball_speed_x, ball_speed_y, ball)
-                print("Gravity -1")
+                if len(balls_list.balls) > 1:
+                    balls_list.change_gravity(balls_list.balls[1].gravity * -1)
+                    print("Gravity Reversed")
+                else:
+                    print("Cant Change gravity, Add more ball by pressing A")
+            if event.key == pygame.K_z:
+                if fps_flag:
+                    fps = 10
+                    fps_flag = False
+                else:
+                    fps = 60
+                    fps_flag = True
+            if event.key == pygame.K_r:
+                balls_list.restart()
+
+        # If you release a button:
         if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                balls_list.balls[0].ball_speed_y -= 10
+                balls_list.balls[0].ball_speed_x = 0
+            if event.key == pygame.K_UP:
+                balls_list.balls[0].ball_speed_y += 10
+                balls_list.balls[0].ball_speed_x = 0
             if event.key == pygame.K_RIGHT:
-                ball_speed_x -= 5
+                balls_list.balls[0].ball_speed_x -= 10
+                balls_list.balls[0].ball_speed_y = 0
             if event.key == pygame.K_LEFT:
-                ball_speed_x += 5
-    ball_object.update(ball_speed_x, ball_speed_y, ball)
-    #ball_speed_x, ball_speed_y = ball_object.ball_animation(ball_object2)
-    ball_speed_x, ball_speed_y=moving_ball(ball_object, ball_object2)
-    ball_speed_x2, ball_speed_y2=moving_ball(ball_object2, ball_object)
-    ball_object2.update(ball_speed_x2,ball_speed_y2, ball_object2.ball)
-    ball_object.update(ball_speed_x, ball_speed_y, ball)
+                balls_list.balls[0].ball_speed_x += 10
+                balls_list.balls[0].ball_speed_y = 0
+            if event.key == pygame.K_a:
+                spawn_flag = False
+
     # Visuals
     screen.fill(bg_color)
-    pygame.draw.ellipse(screen, light_grey, ball)
-    pygame.draw.ellipse(screen, "red", ball2)
-    pygame.draw.aaline(screen, light_grey, (screen_width / 2, 0), (screen_width / 2, screen_height))
-
-    # Text
-    text_speed_x,text_speed_y=update_text(ball_object)
-    text_speed_x2,text_speed_y2=update_text(ball_object2)
-    screen.blit(text_speed_x, textRect_x)
-    screen.blit(text_speed_y, textRect_y)
-    screen.blit(text_speed_x2, textRect_x2)
-    screen.blit(text_speed_y2, textRect_y2)
+    if spawn_flag:
+        balls_list.new_ball_exact_pos(x=random.randint(0, screen_width), y=random.randint(0, screen_height),
+                                      gravity=gravity, radius=random.randint(10, 30),
+                                      color=(
+                                          random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
+                                      screen=screen)
+    balls_list.handle_collisions(mouse_speed_x, mouse_speed_y)
+    main_ball.draw(screen)
+    for ball in balls_list.balls[1:]:
+        ball.draw(screen)
+        ball.move(screen)
 
     # Update Screen
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(fps)
