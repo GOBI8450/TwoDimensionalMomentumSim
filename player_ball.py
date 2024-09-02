4f975e6a35be2c5581a0862809c9e1c43acb51b5
GOBI8450 <148621739+GOBI8450@users.noreply.github.com>
2024-09-02 22:32:47 +0300

    Update player_ball.py

--- player_ball.py
+++ player_ball.py
@@ -1,5 +1,5 @@
-import pygame, random, math
-from base_ball import BASE_BALL
+import pygame, math
+from baseball import BaseBall
 
 pygame.init()
 sound_hit = 'collision_sound.wav'
@@ -7,28 +7,27 @@ pygame.mixer.music.load(sound_hit)
 pygame.mixer.music.set_volume(0.7)
 
 
-class PLAYER_BALL(BASE_BALL):
+class PlayerBall(BaseBall):
 
-    def handle_collision_2d(self, other,mouse_speed_x,mouse_speed_y):
+    def handle_collision_2d(self, other, mouse_speed_x, mouse_speed_y):
         distance, angle = self.distance(other)
         if self.check_collison_2d(other):
             m1, m2 = self.mass, other.mass
-            v1x, v1y = mouse_speed_x,mouse_speed_y
+            v1x, v1y = mouse_speed_x, mouse_speed_y
             v2x, v2y = other.ball_speed_x, other.ball_speed_y
-            if mouse_speed_x==0:
-                u2x=v2x*(-1)
+            if mouse_speed_x == 0:
+                u2x = v2x * (-1)
             else:
                 u2x = ((m2 - m1) * v2x + 2 * m1 * v1x) / (m1 + m2)
-            if mouse_speed_y==0:
-                u2y=v2y*(-1)
+            if mouse_speed_y == 0:
+                u2y = v2y * (-1)
             else:
-                u2y = ((m2 - m1) * v2x + 2 * m1 * v1x) / (m1 + m2)
+                u2y = ((m2 - m1) * v2y + 2 * m1 * v1y) / (m1 + m2)
 
             other.ball_speed_x = u2x
             other.ball_speed_y = u2y
 
             overlap = (self.radius + other.radius) - distance
-            print(angle)
             other.x += overlap * math.cos(angle)
             other.y += overlap * math.sin(angle)
 
